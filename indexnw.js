be3e4e4eb68094378f48832402b5ed437050e7b9
cloudflash <71907815+cloudflash@users.noreply.github.com>
2020-09-27 19:58:00 +0800

    Add files via upload

---
+++ indexnw.js
@@ -0,0 +1,15 @@
+        window.onfocus = function () {
+        document.title = '哇~欢迎回来~(=・ω・=)';
+
+};
+        window.onblur = function () {
+        document.title = '不要抛弃我~(ﾉ’T口T‘)ﾉ ';
+};
+
+
+
+console.info("Hello~");
+
+console.log("这里是XW的网站，欢迎访问！");
+console.log("玩得愉快！");
+
